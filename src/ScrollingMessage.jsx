405bb10b3744ecf089b91d2c2da5253c39281998
4adil-27 <aadilinamdar27@gmail.com>
2024-11-12 17:37:25 +0530

    ScrollingMessage update

--- src/ScrollingMessage.jsx
+++ src/ScrollingMessage.jsx
@@ -5,11 +5,11 @@ function ScrollingMessage() {
     <div className="bg-yellow-400 text-black py-2 overflow-hidden relative">
       <motion.div
         className="flex space-x-8 text-lg font-bold whitespace-nowrap"
-        animate={{ x: [0, -500] }} // Adjust -500 or higher to change scroll length
+        animate={{ x: [0, -800] }} // Adjust -500 or higher to change scroll length
         transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
       >
         <span>Anime, Manga, are being updated!</span>
-        <span>Movies & TV Shows are available now</span>
+        
         <span>
           {" "}
           If Movies/TV Shows aren't loading, use{" "}
